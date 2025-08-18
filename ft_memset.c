fa3adc944af731aac04172314f80f311c3da4ebe
android-varx <andreivartiuc@gmail.com>
2025-08-19 02:30:49 +0300

    maj

--- ft_memset.c
+++ ft_memset.c
@@ -1,3 +1,14 @@
 #include "libft.h"
 
-// TODO: Implement ft_memset
+void *ft_memset(void *s, int x, size_t n)
+{
+    size_t i = 0;
+    unsigned char *m = (unsigned char *)s;
+
+    while (i < n)
+    {
+        m[i] = (unsigned char)x;
+        i++;
+    }
+    return s;
+}
--- libft.h
+++ libft.h
@@ -5,11 +5,18 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-int ft_isalpha(int c);
-int ft_isdigit(int c);
 int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isascii(int c);
+int ft_isdigit(int c);
+int ft_isprint(int c);
+
+void *ft_memset(void *s, int x, size_t n);
 
 size_t ft_strlen(const char *s);
+
+int ft_tolower(int c);
+int ft_toupper(int c);
 
 
 #endif
